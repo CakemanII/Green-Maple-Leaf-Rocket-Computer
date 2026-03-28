7c80fb4e58d1a52028452817073c3a89e33b6ec4
CakemanII <tyler.m.jundt@icloud.com>
2026-03-28 12:31:17 -0400

    test

--- src/lcd_controller.py
+++ src/lcd_controller.py
@@ -13,6 +13,24 @@ class LCDController:
         (0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111),
         (0b11011, 0b00011, 0b00111, 0b01111, 0b11110, 0b11100, 0b11000, 0b10000)
    ]
+    
+    SAD_FACE = [
+        (0b00001, 0b00011, 0b00111, 0b01110, 0b11100, 0b11111, 0b11111, 0b11111)
+        (0b11111, 0b11111, 0b11111, 0b01110, 0b00100, 0b11111, 0b11111, 0b11111)
+        (0b10000, 0b11000, 0b11100, 0b01110, 0b00111, 0b11111, 0b11111, 0b11111)
+        (0b11111, 0b11111, 0b11110, 0b11100, 0b01101, 0b00111, 0b00011, 0b00001)
+        (0b11111, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111)
+        (0b11111, 0b11111, 0b01111, 0b00111, 0b10110, 0b11100, 0b11000, 0b10000)
+    ]
+
+    ANGRY_FACE = [
+        (0b00011, 0b00111, 0b00111, 0b01110, 0b11110, 0b11111, 0b11111, 0b11111)
+        (0b11111, 0b11111, 0b11111, 0b11111, 0b01110, 0b00100, 0b11111, 0b11111)
+        (0b10000, 0b11000, 0b11100, 0b01110, 0b01111, 0b11111, 0b11111, 0b11111)
+        (0b11111, 0b11111, 0b11111, 0b11110, 0b01111, 0b00111, 0b00011, 0b00000)
+        (0b11111, 0b11111, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111, 0b00000)
+        (0b11111, 0b11111, 0b11111, 0b01111, 0b11110, 0b11100, 0b11000, 0b00000)
+    ]
 
     def __init__(self):
         self._emotion_visible = False
@@ -117,5 +135,11 @@ class LCDController:
         self._lcd.backlight_enabled = False
 
 lcd_controller = LCDController()
-lcd_controller.print_emotion(LCDController.SMILEY_FACE, horizontal_position=5)
-lcd_controller.scroll_text("Hello, World!"*2, row=1, delay=0.5)
+for _ in range(3):
+    lcd_controller.print_emotion(LCDController.SMILEY_FACE, horizontal_position=5)
+    time.sleep(1)
+    lcd_controller.print_emotion(LCDController.SAD_FACE, horizontal_position=5)
+    time.sleep(1)
+    lcd_controller.print_emotion(LCDController.ANGRY_FACE, horizontal_position=5)
+    time.sleep(1)
+lcd_controller.scroll_text("Hello, World!"*2, row=1, delay=0.25)
