5f1425f3ab5b5042e48d8a7aed96885a50e672e2
CakemanII <tyler.m.jundt@icloud.com>
2026-04-04 16:21:55 -0400

    123

--- src/controllers/servo_controller.py
+++ src/controllers/servo_controller.py
@@ -28,7 +28,13 @@ class ServoController:
         while True:
             time.sleep(ServoController.SERVO_UPDATE_INTERVAL)
 
-    def set_servo_angle(self, angle: float, delay_for_completion: bool = True):
+    def set_servo_angle(self, angle: float, threaded: bool = True):
+        if threaded:
+            threading.Thread(target=self._do_move_servo, args=(angle,)).start()
+        else:
+            self._do_move_servo(angle)
+
+    def _do_move_servo(self, angle: float):
         """Set servo angle. Supports 0..180 absolute, and negative offsets around midpoint."""
         original_angle = angle
 
@@ -42,9 +48,8 @@ class ServoController:
         duty = 2 + (angle / 18)                  # convert angle to duty cycle
         self._pwm.ChangeDutyCycle(duty)
         self._target_angle = angle    
-        if delay_for_completion:
-            time.sleep(0.3)                           # give servo time to move
-            self._pwm.ChangeDutyCycle(0)             # stop sending signal
+        time.sleep(0.3)                           # give servo time to move
+        self._pwm.ChangeDutyCycle(0)             # stop sending signal
 
     def get_servo_angle(self) -> float:
         """Return the last commanded angle (cannot read actual angle)"""
